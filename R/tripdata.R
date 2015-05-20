db0c533f98e719b08fdfc1bca59e0a9352919e22
Michael Sumner <mdsumner@gmail.com>
2015-05-20 10:22:24 +1000

    data only, package basics

---
+++ R/tripdata.R
@@ -0,0 +1,306 @@
+##' A3.
+##'
+##' A3 taken from the ... package
+##' @name A3
+##' @docType data
+##' @title A3
+##' @keywords data
+NULL
+
+##' albatross.
+##'
+##' albatross taken from the ... package
+##' @name albatross
+##' @docType data
+##' @title albatross
+##' @keywords data
+NULL
+
+##' bear.
+##'
+##' bear taken from the ... package
+##' @name bear
+##' @docType data
+##' @title bear
+##' @keywords data
+NULL
+
+##' buffalo.
+##'
+##' buffalo taken from the ... package
+##' @name buffalo
+##' @docType data
+##' @title buffalo
+##' @keywords data
+NULL
+
+##' capreochiz.
+##'
+##' capreochiz taken from the ... package
+##' @name capreochiz
+##' @docType data
+##' @title capreochiz
+##' @keywords data
+NULL
+
+##' capreotf.
+##'
+##' capreotf taken from the ... package
+##' @name capreotf
+##' @docType data
+##' @title capreotf
+##' @keywords data
+NULL
+
+##' condor.
+##'
+##' condor taken from the ... package
+##' @name condor
+##' @docType data
+##' @title condor
+##' @keywords data
+NULL
+
+##' deer.
+##'
+##' deer taken from the ... package
+##' @name deer
+##' @docType data
+##' @title deer
+##' @keywords data
+NULL
+
+##' dugong.
+##'
+##' dugong taken from the ... package
+##' @name dugong
+##' @docType data
+##' @title dugong
+##' @keywords data
+NULL
+
+##' georef1min01.
+##'
+##' georef1min01 taken from the ... package
+##' @name georef1min01
+##' @docType data
+##' @title georef1min01
+##' @keywords data
+NULL
+
+##' georef1min02.
+##'
+##' georef1min02 taken from the ... package
+##' @name georef1min02
+##' @docType data
+##' @title georef1min02
+##' @keywords data
+NULL
+
+##' georef1min03.
+##'
+##' georef1min03 taken from the ... package
+##' @name georef1min03
+##' @docType data
+##' @title georef1min03
+##' @keywords data
+NULL
+
+##' georef1min26.
+##'
+##' georef1min26 taken from the ... package
+##' @name georef1min26
+##' @docType data
+##' @title georef1min26
+##' @keywords data
+NULL
+
+##' georef1min95.
+##'
+##' georef1min95 taken from the ... package
+##' @name georef1min95
+##' @docType data
+##' @title georef1min95
+##' @keywords data
+NULL
+
+##' gpsdata01.
+##'
+##' gpsdata01 taken from the ... package
+##' @name gpsdata01
+##' @docType data
+##' @title gpsdata01
+##' @keywords data
+NULL
+
+##' gpsdata02.
+##'
+##' gpsdata02 taken from the ... package
+##' @name gpsdata02
+##' @docType data
+##' @title gpsdata02
+##' @keywords data
+NULL
+
+##' gpsdata03.
+##'
+##' gpsdata03 taken from the ... package
+##' @name gpsdata03
+##' @docType data
+##' @title gpsdata03
+##' @keywords data
+NULL
+
+##' gpsdata26.
+##'
+##' gpsdata26 taken from the ... package
+##' @name gpsdata26
+##' @docType data
+##' @title gpsdata26
+##' @keywords data
+NULL
+
+##' gpsdata95.
+##'
+##' gpsdata95 taken from the ... package
+##' @name gpsdata95
+##' @docType data
+##' @title gpsdata95
+##' @keywords data
+NULL
+
+##' hseal.
+##'
+##' hseal taken from the ... package
+##' @name hseal
+##' @docType data
+##' @title hseal
+##' @keywords data
+NULL
+
+##' ibex.
+##'
+##' ibex taken from the ... package
+##' @name ibex
+##' @docType data
+##' @title ibex
+##' @keywords data
+NULL
+
+##' ibexraw.
+##'
+##' ibexraw taken from the ... package
+##' @name ibexraw
+##' @docType data
+##' @title ibexraw
+##' @keywords data
+NULL
+
+##' leroy.
+##'
+##' leroy taken from the ... package
+##' @name leroy
+##' @docType data
+##' @title leroy
+##' @keywords data
+NULL
+
+##' locations.
+##'
+##' locations taken from the ... package
+##' @name locations
+##' @docType data
+##' @title locations
+##' @keywords data
+NULL
+
+##' mouflon.
+##'
+##' mouflon taken from the ... package
+##' @name mouflon
+##' @docType data
+##' @title mouflon
+##' @keywords data
+NULL
+
+##' panda.
+##'
+##' panda taken from the ... package
+##' @name panda
+##' @docType data
+##' @title panda
+##' @keywords data
+NULL
+
+##' porpoise.
+##'
+##' porpoise taken from the ... package
+##' @name porpoise
+##' @docType data
+##' @title porpoise
+##' @keywords data
+NULL
+
+##' puechcirc.
+##'
+##' puechcirc taken from the ... package
+##' @name puechcirc
+##' @docType data
+##' @title puechcirc
+##' @keywords data
+NULL
+
+##' ricky.
+##'
+##' ricky taken from the ... package
+##' @name ricky
+##' @docType data
+##' @title ricky
+##' @keywords data
+NULL
+
+##' rupicabau.
+##'
+##' rupicabau taken from the ... package
+##' @name rupicabau
+##' @docType data
+##' @title rupicabau
+##' @keywords data
+NULL
+
+##' seal.
+##'
+##' seal taken from the ... package
+##' @name seal
+##' @docType data
+##' @title seal
+##' @keywords data
+NULL
+
+##' Simp.
+##'
+##' Simp taken from the ... package
+##' @name Simp
+##' @docType data
+##' @title Simp
+##' @keywords data
+NULL
+
+##' teal.
+##'
+##' teal taken from the ... package
+##' @name teal
+##' @docType data
+##' @title teal
+##' @keywords data
+NULL
+
+##' whale.
+##'
+##' whale taken from the ... package
+##' @name whale
+##' @docType data
+##' @title whale
+##' @keywords data
+NULL
+
---
+++ inst/fetchdata/fetch.R
@@ -0,0 +1,6 @@
+# pkgs <- c("animalTrack", "argosfilter", "BBMM", "bcpa", "changepoint",
+#   "fishmove", "GeoLight", "move", "smam", "TrackReconstruction",
+#   "trajectories", "wildlifeDI")
+#
+# for (i in seq_along(pkgs)) install.packages(pkgs[i])
+#
